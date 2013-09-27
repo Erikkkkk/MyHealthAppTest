391079f7baa1028cd667789b156dcb6fce280750
erikkkk <ei.slim@st.hanze.nl>
2013-09-27 14:46:26 +0200

    Main activity test case setup

--- src/com/jcheed06/myhealthapp/test/Bluetoothtest.java
+++ src/com/jcheed06/myhealthapp/test/Bluetoothtest.java
@@ -4,4 +4,14 @@ import junit.framework.TestCase;
 
 public class Bluetoothtest extends TestCase {
 
+	public Bluetoothtest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	
+
 }
---
+++ src/com/jcheed06/myhealthapp/test/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.jcheed06.myhealthapp.test;
+
+import com.jcheed06.myhealthapp.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	//private TextView result;
+
+	public MainActivityTest() {
+		super("com.jcheed06.myhealthapp", MainActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		MainActivity mainActivity = getActivity();
+		//result = (TextView) mainActivity.findViewById(R.id.result);  
+	}
+
+}
