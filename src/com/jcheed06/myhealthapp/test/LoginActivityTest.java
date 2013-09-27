aa90d4cb6857916ebbce2061ec29d4d5cd4587b0
erikkkk <ei.slim@st.hanze.nl>
2013-09-27 15:21:46 +0200

    Attempt to send login form data

--- src/com/jcheed06/myhealthapp/test/LoginActivityTest.java
+++ src/com/jcheed06/myhealthapp/test/LoginActivityTest.java
@@ -4,9 +4,10 @@ import com.jcheed06.myhealthapp.LoginActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-public class LoginActivityTest extends
-		ActivityInstrumentationTestCase2<LoginActivity> {
-	
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+	private static final String CORRECT_USERNAME = "cristianhalman";  
+	private static final String INCORRECT_PASSWORD = "lblalababllabla";  
+	private static final String CORRECT_PASSWORD = "password";  
 
 	public LoginActivityTest() {
 		super("com.jcheed06.myhealthapp", LoginActivity.class);
@@ -16,4 +17,15 @@ public class LoginActivityTest extends
 		super.setUp();
 	}
 
+	public void testIncrementIncorrectLoginsIntIfCredentialsWrong() {  
+		   sendKeys(CORRECT_USERNAME);
+		   sendKeys("ENTER");
+		   sendKeys(INCORRECT_PASSWORD);
+		   sendKeys("ENTER");
+		   
+		   
+		   // get result  
+		   //String mathResult = result.getText().toString();  
+		   //assertTrue("Add result should be 98", mathResult.equals(ADD_RESULT));  
+		}  
 }
